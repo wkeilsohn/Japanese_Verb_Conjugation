e290a20695b0759596aef9b8ab5a3b8589acd5f5
wkeilsohn <kingwilliam42@gmail.com>
2025-12-03 15:06:15 +0900

    Created Main File

---
+++ main.py
@@ -0,0 +1,20 @@
+# William Keilsohn
+# December 3 2025
+
+# Import Packages
+import os
+import numpy as np
+import pandas as pd
+
+# Delcare Global Variables
+ru_verb_end_char = "る"
+exception_ls = ["する", "くる", "ある"]
+
+
+
+# Define Functions
+
+# Run Application
+
+if __name__=="__main__":
+    print("Hello World")
